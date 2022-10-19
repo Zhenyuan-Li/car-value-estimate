346a170a2fa3066ed150501a76c2db0fd07ba39c
Vincent0Lee <vincent0lee95@gmail.com>
2022-10-19 21:40:36 +0800

    migration bugs fixed

--- migrations/1666185294040-initial-schema.js
+++ migrations/1666185294040-initial-schema.js
@@ -1,8 +1,8 @@
 const { MigrationInterface, QueryRunner, Table } = require('typeorm');
- 
+
 module.exports = class initialSchema1625847615203 {
   name = 'initialSchema1625847615203';
- 
+
   async up(queryRunner) {
     await queryRunner.createTable(
       new Table({
@@ -31,3 +31,34 @@ module.exports = class initialSchema1625847615203 {
         ],
       }),
     );
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'report',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+          },
+          { name: 'approved', type: 'boolean', default: 'false' },
+          { name: 'price', type: 'float' },
+          { name: 'make', type: 'varchar' },
+          { name: 'model', type: 'varchar' },
+          { name: 'year', type: 'integer' },
+          { name: 'lng', type: 'float' },
+          { name: 'lat', type: 'float' },
+          { name: 'mileage', type: 'integer' },
+          { name: 'userId', type: 'integer' },
+        ],
+      }),
+    );
+  }
+
+  async down(queryRunner) {
+    await queryRunner.query(`DROP TABLE ""report""`);
+    await queryRunner.query(`DROP TABLE ""user""`);
+  }
+};
