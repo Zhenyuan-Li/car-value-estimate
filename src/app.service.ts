3fe0593d67f272ddd2249c785a6c508507815a5b
Vincent0Lee <vincent0lee95@gmail.com>
2022-10-20 22:16:23 +0800

    update default route

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  getHello(): string {
+    return '<h1>Welcome</h1><h2><a href="https://vincent-car-value.herokuapp.com/">See the Swagger API Document</a><h2>';
+  }
+}
